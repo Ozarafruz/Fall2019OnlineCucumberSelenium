570831440bc063103e2f7da2f814b7c4ab2e2911
ozarafruz <ozarafruz@gmail.com>
2020-04-15 19:52:29 -0400

    Cucumber Scenarios

--- src/test/java/com/vytrack/page/LoginPage.java
+++ src/test/java/com/vytrack/page/LoginPage.java
@@ -1,8 +1,9 @@
 package com.vytrack.page;
 
-import com.automation.utilities.BrowserUtils;
-import com.automation.utilities.ConfigurationReader;
-import com.automation.utilities.Driver;
+
+import com.vytrack.utilities.BrowserUtilities;
+import com.vytrack.utilities.ConfigurationReader;
+import com.vytrack.utilities.Driver;
 import org.openqa.selenium.Keys;
 import org.openqa.selenium.WebElement;
 import org.openqa.selenium.support.FindBy;
@@ -46,7 +47,7 @@ public class LoginPage {
     public void login(String usernameValue, String passwordValue){
         username.sendKeys(usernameValue);
         password.sendKeys(passwordValue, Keys.ENTER);
-        BrowserUtils.wait(3);
+        BrowserUtilities.wait(3);
     }
 
     /**
